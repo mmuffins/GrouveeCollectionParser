c6e763ae55b9689557b06171e7c5393aaeb469ca
mmuffins <mail@tiv73.com>
2018-05-27 08:54:32 +0200

    Added basic project structure

--- GrouveeParser/GrouveeGame.cs
+++ GrouveeParser/GrouveeGame.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GrouveeParser
 {
-    public class Class1
+    public class GrouveeGame
     {
     }
 }
---
+++ GrouveeParserConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using GrouveeParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrouveeParserConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(async () => await ImportCollection()).Wait();
+        }
+
+        static async Task ImportCollection()
+        {
+
+        }
+    }
+}
